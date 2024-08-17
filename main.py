9772a61cefd00115f59efe7c64c6e4fbe00e2aa6
Павел Разбойников <jan333pan@mail.ru>
2024-08-17 22:05:42 +0700

    1

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def send_message():
     global message_id  # Используем глобальную переменную
     moscow_tz = pytz.timezone('Europe/Moscow')
     moscow_time = datetime.now(moscow_tz)
-    if moscow_time.hour == 17:  # Отправляем сообщение в 11:57 МСК
+    if moscow_time.hour == 18:  # Отправляем сообщение в 11:57 МСК
         channel = bot.get_channel(config['channel_id'])
         if channel:
             embed = await create_initial_embed()
